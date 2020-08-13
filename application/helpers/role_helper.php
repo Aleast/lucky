6292759cc145bca8c82843536ba4e11561267529
kwor <kwor@kwordeMacBook-Pro.local>
2020-08-13 22:17:52 +0800

    1

--- application/helpers/role_helper.php
+++ application/helpers/role_helper.php
@@ -2,8 +2,9 @@
 //权限判断辅助方法
 //调用方法
 //$this->load->helper('role');
+//然后可以直接使用当中的函数
 /*
-$rilenum 权限数字如：1001，字符串类型
+$rilenum 权限数字如：1001，字符串类型，四个位置对应增删改查
 $position 指定数字的位置，下标0开始,
 */
 function get_role($rilenum,$position){
